7bc5fa97e5bf5952c95953f97943f63f35e29b3f
terryweis <142371601+terryweis@users.noreply.github.com>
2023-08-16 19:59:03 -0500

    hello.js
    Modified and changed the message finally landing on The Game is Afoot

--- hello.js
+++ hello.js
@@ -4,7 +4,13 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+const num1 = 8;
+const num2 = 9;
+const sum = num1 + num2;
+
+console.log("The Game is a Afoot");
+
+
 
 /*  
 Next, PLAY! 
